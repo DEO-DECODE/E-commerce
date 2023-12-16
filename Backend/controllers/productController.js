081154c8d4ecc33948077ae0f8a952f8e88fe7e1
Deo Raj <d.jrajsingh81@gmail.com>
2023-12-16 17:51:08 +0530

    Update Product By Admin added

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -5,7 +5,7 @@ exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
-    product
+    product,
   });
 };
 // Get all Products
@@ -14,5 +14,25 @@ exports.getAllProducts = async (req, res) => {
   res.status(200).json({
     success: true,
     products,
+  });
+};
+
+// Update a product
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
   });
 };
