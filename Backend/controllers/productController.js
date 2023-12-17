2a6079d2d4dd157de9a81d5384a5703a15d88adb
Deo Raj <d.jrajsingh81@gmail.com>
2023-12-17 19:30:49 +0530

    Delete Functionality added for admin

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -36,3 +36,18 @@ exports.updateProduct = async (req, res, next) => {
     product,
   });
 };
+// Delete a Product by Admin
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+  await product.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Product Deleted Successfully",
+  });
+};
