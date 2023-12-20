a8c3c2e7be35fb42f2eba391000a74b1271a98db
Deo Raj <d.jrajsingh81@gmail.com>
2023-12-20 16:35:53 +0530

    Login UserDone and Error Resolved

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -1,17 +1,25 @@
-// Create Token and saving in cookie
-
 const sendToken = (user, statusCode, res) => {
+  // Generate a JWT token for the user
   const token = user.getJWTToken();
 
-  // options for cookie
+  // Parse the COOKIE_EXPIRE value into a number
+  const expiresInDays = parseInt(process.env.COOKIE_EXPIRE, 10);
+
+  if (isNaN(expiresInDays) || expiresInDays <= 0) {
+    console.error("Invalid COOKIE_EXPIRE value:", process.env.COOKIE_EXPIRE);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal Server Error" });
+  }
+
+  // Calculate the expiration time for the cookie
+  const expiresInMilliseconds = expiresInDays * 24 * 60 * 60 * 1000;
+  const expirationDate = new Date(Date.now() + expiresInMilliseconds);
+
+  // Options for the cookie
   const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
+    expires: expirationDate,
     httpOnly: true,
-    /*
-    httpOnly: This option is set to true, making the cookie accessible only through HTTP/S protocols. This is a security measure to help prevent client-side access to the cookie through JavaScript, reducing the risk of cross-site scripting (XSS) attacks.
-    */
   };
 
   res.status(statusCode).cookie("token", token, options).json({
@@ -19,12 +27,17 @@ const sendToken = (user, statusCode, res) => {
     user,
     token,
   });
-  /*
-  .cookie("token", token, options): Sets a cookie in the response. It takes three parameters:
 
-"token": The name of the cookie.
-token: The actual value of the cookie, which is the JWT token in this case.
-options: The options object defined earlier, specifying expiration time and HTTP-only.
+  // Options for the cookie
+
+  // httpOnly: Set to true to make the cookie accessible only through HTTP/S protocols, preventing client-side access through JavaScript and reducing the risk of XSS attacks.
+  // Options for the cookie
+
+  /*
+    .cookie("token", token, options): Sets a cookie in the response. It takes three parameters:
+    "token": The name of the cookie.
+    token: The actual value of the cookie, which is the JWT token in this case.
+    options: The options object defined earlier, specifying expiration time and HTTP-only.
   */
 };
 
