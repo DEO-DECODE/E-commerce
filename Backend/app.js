98ad789ee92123ffd75cbb26bb456cf93b04fee2
Deo Raj <d.jrajsingh81@gmail.com>
2023-12-23 02:53:48 +0530

    Done With The  Order

--- Backend/app.js
+++ Backend/app.js
@@ -7,8 +7,10 @@ app.use(cookieParser());
 // Route imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoutes");
+const order= require("./routes/orderRoute")
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 // Middleware for error
 app.use(errorMiddleware);
 module.exports = app;
